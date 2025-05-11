b948ec8108b2c704eff27360c364c4787158361d
diegobarbosa <diegobarbosadev@gmail.com>
2025-05-10 23:44:21 -0300

    antes de alterar o CMD do Dockerfile

---
+++ src/core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
